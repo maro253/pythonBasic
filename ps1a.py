091f17bff421fcc970543bc392be5bc0fd1cbd36
maro253 <marok253@gmail.com>
2013-12-11 17:27:20 -0800

    Create ps1a.py

---
+++ ps1a.py
@@ -0,0 +1,14 @@
+# Problem Set 1a
+# maro253
+# Time Spent: 3:30
+# 
+outBalance = double(raw_input("Enter the out standing balance on your credit card: "))
+annInterest = double(raw_input("Enter the annual credit card interest rate as a decimal: "))
+minMonRate = double(raw_input("Enter the minimum monthly payment rate as a decimal: "))
+
+minMonPayment = minMonRate * outBalance
+principalPaid = 
+
+print "Month: 1"
+print "Minimum monthly payment: " + minMonPayment
+print "Principal paid: "
